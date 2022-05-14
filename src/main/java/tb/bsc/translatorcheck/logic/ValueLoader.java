992035ddef5c1c4b93fbf4ad1e6664c6fe8e56b1
dalailama <idea@3dots.ch>
2022-05-14 07:27:53 +0200

    change Path using of RessourceFile..... for Testpurpose its ok

--- src/main/java/tb/bsc/translatorcheck/logic/ValueLoader.java
+++ src/main/java/tb/bsc/translatorcheck/logic/ValueLoader.java
@@ -3,20 +3,23 @@ package tb.bsc.translatorcheck.logic;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
+import tb.bsc.translatorcheck.TranslatorApplication;
 import tb.bsc.translatorcheck.dto.Eval;
 import tb.bsc.translatorcheck.dto.Vocable;
 import tb.bsc.translatorcheck.dto.Vocabulary;
 
 import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 import java.util.List;
 
 public class ValueLoader {
 
     public List<Vocable> loadData() {
         try {
-            File file = new File("C:\\BPS\\TranslatorCheck\\data.json");
 
+            URL url =  TranslatorApplication.class.getResource("data.json");
+            File file = new File(url.toURI());
             GsonBuilder builder = new GsonBuilder();
             Gson gson = builder.create();
             JsonReader reader = new JsonReader(new FileReader(file));
